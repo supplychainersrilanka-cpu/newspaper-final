0f324151c996669a7d346bddd7e3a94a276ef41c
NinjaCommando <bnuwan123@@gmail.com>
2025-12-29 19:40:32 +0530

    Verified stable build: Next.js 15.1.0

---
+++ lib/prisma.ts
@@ -0,0 +1,13 @@
+﻿import { PrismaClient } from '@prisma/client'
+import { PrismaD1 } from '@prisma/adapter-d1'
+import { getRequestContext } from '@cloudflare/next-on-pages'
+
+const prismaClientSingleton = () => {
+  const runtime = getRequestContext().env;
+  const adapter = new PrismaD1(runtime.DB);
+  return new PrismaClient({ adapter });
+}
+declare global { var prisma: undefined | ReturnType<typeof prismaClientSingleton> }
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+export default prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
